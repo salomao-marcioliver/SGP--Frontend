0080859510551434f29029180df17dca930c9a23
G. Diana <gab13ssousa@gmail.com>
2025-05-14 20:29:22 -0300

    Login page updated

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -15,21 +15,24 @@ const LoginPage = () => {
   }
 
   return (
-    <div id="login">
-      <h1 className="title">Login do sistema</h1>
-      <form className="form" onSubmit={handleSubmit}>
-        <div className="field">
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-        </div>
-        <div className="field">
-          <label htmlFor="password">Senha</label>
-          <input type="password" name="password" id="password" value={password} onChange={(e) => setPassoword(e.target.value)}/>
-        </div>
-        <div className="actions">
-          <button type="submit">Entrar</button>
-        </div>
-      </form>
+    <div className="container" id="login">
+      <div className="form-box">
+
+        <h1>LOGIN</h1>
+
+        <form className="login-form" onSubmit={handleSubmit}>
+          <input className="form-input" type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)}
+          placeholder="E-mail" required/>
+          <input className="form-input" type="password" name="password" id="password" value={password} onChange={(e) => setPassoword(e.target.value)} placeholder="Senha" required/>
+
+          <button className="form-btn" type="submit">Entrar</button>
+
+        </form>
+
+        <p class="text"><a href="#">Esqueceu a senha?</a> ou <a href="#">Cadastro</a> </p>
+
+      </div>
+      
     </div>
   )
 }
